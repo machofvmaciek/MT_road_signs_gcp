09fabeccfd8869c53cc585bdc316d5fbb96f9b96
machofvmaciek <olszewski.maciej.mo@gmail.com>
2024-07-14 18:56:07 +0200

    Modified rs-recognize to return class name instead of index

--- rs-recognize/main.py
+++ rs-recognize/main.py
@@ -1,4 +1,3 @@
-
 import json
 import functions_framework
 
@@ -10,6 +9,9 @@ from google.cloud import storage
 # Replace with your bucket and file names
 BUCKET_NAME = 'bucket-road-signs'
 FILE_MODEL = 'cnn_v8_ep_18_tensor_16.h5'
+
+CLASSES = ['Speed limit (20km/h)', 'Speed limit (30km/h)', 'Speed limit (50km/h)', 'Speed limit (60km/h)', 'Speed limit (70km/h)', 'Speed limit (80km/h)', 'End of speed limit (80km/h)', 'Speed limit (100km/h)', 'Speed limit (120km/h)', 'No passing', 'No passing for vehicles over 3.5 metric tons', 'Right-of-way at the next intersection', 'Priority road', 'Yield', 'Stop', 'No vehicles', 'Vehicles over 3.5 metric tons prohibited', 'No entry', 'General caution', 'Dangerous curve to the left', 'Dangerous curve to the right', 'Double curve', 'Bumpy road', 'Slippery road', 'Road narrows on the right', 'Road work', 'Traffic signals', 'Pedestrians', 'Children crossing', 'Bicycles crossing', 'Beware of ice/snow', 'Wild animals crossing', 'End of all speed and passing limits', 'Turn right ahead', 'Turn left ahead', 'Ahead only', 'Go straight or right', 'Go straight or left', 'Keep right', 'Keep left', 'Roundabout mandatory', 'End of no passing', 'End of no passing by vehicles over 3.5 metric tons'] 
+
 model = None
 
 @functions_framework.http
@@ -24,9 +26,6 @@ def predict(request):
         blob.download_to_filename('/tmp/model.h5')
         model = tf.keras.models.load_model('/tmp/model.h5')
     
-    # response = json.dumps(model.summary())
-    # return response
-    
     if request.method == "POST":
         try:
             request_json = request.get_json()
@@ -39,22 +38,12 @@ def predict(request):
 
             # Convert list to ndarray
             img = np.array(data_list)
+
         except (json.JSONDecodeError, ValueError, KeyError) as e:
             return f'Error decoding or converting ndarray: {e}', 400
         
         probabilities = model.predict(img)
-        prediction = np.argmax(probabilities, axis=1)
+        # prediction = np.argmax(probabilities, axis=1)
+        prediction = CLASSES[np.argmax(probabilities, axis=1)[0]]
 
-        return json.dumps({"prediction": prediction.tolist()})
-    
-    # # Get input data from the request (JSON or query parameters)
-    # input_data = request.get_json()
-    # # Preprocess input_data as needed
-
-    # # Make predictions
-    # predictions = model.predict(input_data)
-
-    # # Postprocess predictions as needed (e.g., convert to desired format)
-
-    # # Return the predictions in the response
-    # return {'predictions': predictions.tolist()}  # Convert to list for JSON
+        return json.dumps({"prediction": prediction})
